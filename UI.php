bc38fcccc292339b9c6241a282bbd97c40e40347
Trenclik <trenclikb1@gmail.com>
2024-02-28 22:47:13 +0100

    bruzh

---
+++ UI.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="UI_style.css">
+</head>
+<body>
+    <header>
+        <a href="index.php"><img src="/obrázky/logo.png" alt=""></a>
+
+    </header>
+
+    <nav>
+        <div class="odkazy">
+            <a id="a" href="#">Obec</a>
+            <a id="a" href="#">Úřad</a>
+            <a id="a" href="#">Aktuálně</a>
+            <a id="a" href="#">Fotogalerie</a>
+            <a id="a" href="#">Kontakt</a>
+        </div>
+        <div class="search">
+            <input type="text">
+        </div>
+    </nav>
+</body>
+</html>
