f494089bcff692df70efbdd9797ec3d1df907033
Trenclik <trenclikb1@gmail.com>
2024-04-19 14:08:43 +0200

    nevim vole už funguje všechno asi

--- UI.php
+++ UI.php
@@ -1,7 +1,8 @@
     <link rel="stylesheet" href="UI_style.css">
+    <script src="UI_script.js"></script>
 </head>
 <body>
-    <div id="dropdown" style="visibility: hidden" >
+    <div id="dropdown" style="opacity: 0%; visibility: hidden;" >
             <button type="button" onclick="nevidim()" class="hamba"><svg class="mobile_UI" viewBox="0 0 24 24" fill="none" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 18L20 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path> <path d="M4 12L20 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path> <path d="M4 6L20 6" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path></g></svg></button>
             <div class="dropdown-menu">
                 <a href="#">Obec</a>
@@ -28,13 +29,5 @@
         </div>
 
         <button type="button" onclick="vidim()" class="hamba"><svg class="mobile_UI" viewBox="0 0 24 24" fill="none" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 18L20 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path> <path d="M4 12L20 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path> <path d="M4 6L20 6" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path></g></svg></button>
-        <script>
-        function vidim() {document.getElementById('dropdown').style.visibility='visible'; disableScroll();}
-        function nevidim() {document.getElementById('dropdown').style.visibility='hidden'; enableScroll();}
-    </script>
-        <script>
-        function disableScroll() {document.body.classList.add("zastav");}
-        function enableScroll() {document.body.classList.remove("zastav");}
-    </script>
     </nav>
     
