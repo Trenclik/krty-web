ecc168f79718f8463837bc95018c800803b95b0e
Trenclik <trenclikb1@gmail.com>
2024-05-17 09:09:11 +0200

    final
    pro prezentaci finální verze

--- index.php
+++ index.php
@@ -6,18 +6,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Obec Krty</title>
     <link rel="stylesheet" href="global.css">
+    <link rel="stylesheet" href="kalendar.css">
 <?php 
     include 'UI.php'
 ?>
 
     <main>
         <div class="banner">
-            bruh
+            <?php
+            include 'kalendar.php'
+            ?>
+            <div class="novinky">
+                <img src="/obrázky\DSC_0699.jpg">
+                <div>
+                    <h3>Přívod k rybníku</h3>
+                    <p>Byl zřízen nový přítok k rybníku.</p>
+                </div>
+            </div>
+            <div class="novinky">
+                <img src="/obrázky\20190929_135955.jpg">
+                <div>
+                    <h3>Vodník v Krtníku</h3>
+                    <p>U rybníka je niní uazený vodník.</p>
+                </div>
+            </div>
         </div>
         <div class="stred">
             <h1>Vítejte v Krtech</h1>
             <div class="obsah">
-                <p></p>
+                <h2>Pár slov úvodem</h2>
+                <p>Obec Krty leží v&nbsp;439m nadmořské výšky, nejvyšší vrchol Krtských skal je vysoký 542m.</p> <p>Původ obce Krty je ryze Slovanský - typická je okrouhlice.<br> Jméno obce jest podle pověsti jménem jistého čeledína. Stařešina rodu se&nbsp;jmenoval Krt&nbsp;od&nbsp;slovanského jména a&nbsp;tudy povstalo jméno Krty.</p>
+                <br>
+                <h2>Historie</h2>
+                <p>Původ obce Krty je ryze Slovanský -&nbsp; typická je okrouhlice.<br> Jméno obce jest podle pověsti jménem jistého čeledína. Stařešina rodu se&nbsp;jmenoval Krt od&nbsp;slovanského jména a&nbsp;tudy povstalo jméno Krty.</p> <p>Není žádných zpráv, že&nbsp;by obec Krty byla osídlena již v&nbsp;pravěku, tehdejších dávných časů vystupují Krty až&nbsp;v&nbsp;roce 1227, kdy patřila nynější obec ku hradu Rabštejnu. Tehdy byla majetkem Libštejnu z&nbsp;Kolovrat. Po&nbsp;smrti Jaroslava staršího Libštejnského z&nbsp;Kolovrat přešly jako děditství jeho synu Benediktovi.</p>
+            </div>
+            <div class="aktu">
+                <h1>Aktuality</h1>
+                <div class="bruh">
+                    <a href="#"><h2>Veřejná vyhláška - Finanční úřad</h2></a>
+                    <p>30.04.2024 - 31.05.2024</p>
+                </div>
+                <div class="bruh">
+                    <a href="#"><h2>Opatření obecné povahy - Stanovení přechodné úpravy provozu na pozemních komunikacích</h2></a>
+                    <p>30.04.2024 - 20.05.2024</p>
+                </div>
+                <div class="bruh">
+                    <a href="#"><h2>Porovnání všech položek výpočtu ceny pro VaS za rok 2023</h2></a>
+                    <p>30.04.2024 - 31.05.2024</p>
+                </div>
+                <div class="bruh">
+                    <a href="#"><h2>Veřejná vyhláška - Oznámení o zahájení společného jednání - Domovní ČOV</h2></a>
+                    <p>29.04.2024 - 28.05.2024</p>
+                </div>
             </div>
         </div>
 
---
+++ kontakt.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontakt</title>
+    <link rel="stylesheet" href="global.css">
+<?php 
+    include 'UI.php'
+?>
+
+    <main>
+        
+    </main>
+    <?php 
+        include 'UI_foot.php'
+    ?>
+    
+</body>
+</html>
