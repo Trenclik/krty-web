4c94f9cb340882c01ef195df7f81501872c34063
Trenclik <trenclikb1@gmail.com>
2025-01-07 16:40:45 +0100

    Přechod na svelte.kit
    Tímto commitem začíná přechod na framework svelte.kit.
    Předchozí php verze byla přesunuta na větev legacy-php.

--- UI_script.js
+++
@@ -1,9 +0,0 @@
-function disableScroll() {document.body.classList.add("zastav");}
-function enableScroll() {document.body.classList.remove("zastav");}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function vidim() {document.getElementById('dropdown').style.opacity='100%'; disableScroll(); document.getElementById('dropdown').style.visibility='visible';}
-async function nevidim() {document.getElementById('dropdown').style.opacity='0%'; enableScroll(); await sleep(500);document.getElementById('dropdown').style.visibility='collapse';}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Footer from "$lib/Components/Footer.svelte";
+    import Header from "$lib/Components/Header.svelte";
+    let { data, children } = $props();
+  </script>
+  
+  <Header/>
+  
+  <main>
+    {@render children()}
+  </main>
+  
+  <Footer data={data} />
